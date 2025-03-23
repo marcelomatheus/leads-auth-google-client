5ca8292f9499a074342eed149c3def39530f7a0c
Marcelo Oliveira <marcelomatheusbr@gmail.com>
2025-03-22 21:59:47 -0300

    feat: add logout button to home page for user authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+import { useAuth } from "./context/Auth-Context";
 
 export default function Home() {
+  const contextAuth = useAuth();
   return (
     <div className="bg-dark  w-[600px]">
       <h1 className="font-bold text-3xl flex font-heading">Home</h1>
+      <button onClick={() => contextAuth?.dispatch({ type: "LOGOUT" })}>
+        Logout
+      </button>
     </div>
   );
 }
