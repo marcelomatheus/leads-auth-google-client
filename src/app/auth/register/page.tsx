af7d1c1ee13df1e16ac0bd59c2c26d4e389db12a
Marcelo Oliveira <marcelomatheusbr@gmail.com>
2025-03-22 21:58:02 -0300

    feat: add registration page

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,5 +1,5 @@
 import { FormRegister } from "./form-register";
-const ContactPage: React.FC = () => {
+const Register: React.FC = () => {
   return (
     <div className="flex flex-row w-full h-screen justify-center items-center bg-[url('../../assets/city.jpg')] bg-cover bg-blend-color bg-black/75 ">
       <FormRegister />
@@ -7,4 +7,4 @@ const ContactPage: React.FC = () => {
   );
 };
 
-export default ContactPage;
+export default Register;
