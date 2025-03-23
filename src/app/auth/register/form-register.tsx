8fd38d1061e582710072762e59cdf7ab95928a27
Marcelo Oliveira <marcelomatheusbr@gmail.com>
2025-03-22 21:56:07 -0300

    feat: implement registration form with validation and Google login

--- src/app/auth/register/form-register.tsx
+++ src/app/auth/register/form-register.tsx
@@ -5,16 +5,21 @@ import {
   FormMessageError,
   Input,
   InputIcon,
-} from "@/components/Form";
+} from "@/components/ui/Form";
 import { ComponentProps, useState } from "react";
 import { Eye, EyeClosed, Lock, LockKeyhole, Mail, User } from "lucide-react";
-import { Button } from "@/components/Button";
+import { Button } from "@/components/ui/Button";
 import Image from "next/image";
 import Google from "@/Google.svg";
 import { FieldErrors, useForm } from "react-hook-form";
 import { useGoogleLogin } from "@react-oauth/google";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { userLogin, userRegister } from "@/app/auth/_service/auth-service";
+import { useAuth } from "@/app/context/Auth-Context";
+import { useRouter } from "next/navigation";
+import { AxiosError } from "axios";
 
 export const formSchema = z
   .object({
@@ -32,6 +37,7 @@ export const formSchema = z
     message: "Você deve concordar com a política de privacidade",
     path: ["confirmPolicy"],
   });
+
 type Form = z.infer<typeof formSchema>;
 
 export const FormRegister = () => {
@@ -40,12 +46,40 @@ export const FormRegister = () => {
     handleSubmit,
     formState: { errors },
   } = useForm({ resolver: zodResolver(formSchema) });
-  console.log(errors);
-  const onSubmit = (data: Form) => console.log(data);
+  const [errorAccess, setErrorAccess] = useState<{message: string}>();
+
+  const router = useRouter();
+  
+  const contextLogin = useAuth();
+
+  const onSubmit = async (payload: Form) => {
+    try {
+      const userRegisterData = await userRegister(payload);
+      const userLoginData = await userLogin({
+        email: userRegisterData.data.email,
+        password: payload.password,
+      });
+      if (userLoginData.data.token) {
+        contextLogin?.dispatch({
+          type: "LOGIN",
+          payload: { token: userLoginData.data.token },
+        });
+        router.push("/");
+      }
+    } catch (error) {
+      const err = (error as AxiosError<{
+            message: string;
+          }>);
+      const message = String(err?.response?.data?.message) || "Erro ao fazer login";
+      setErrorAccess({message});
+    }
+  };
+
   const login = useGoogleLogin({
     onSuccess: (response) => console.log(response),
     onError: (error) => console.error(error),
   });
+
   return (
     <div className="flex mx-auto h-screen flex-col justify-center items-center">
       <Form onSubmit={handleSubmit(onSubmit)} className="md:w-[400px]">
@@ -99,9 +133,14 @@ export const FormRegister = () => {
         </Button>
         <div className="flex mx-auto gap-1">
           Já tem uma conta?
-          <a className="underline text-gray-100 cursor-pointer">Faça login</a>
+          <Link
+            href={"/auth/login"}
+            className="underline text-gray-100 cursor-pointer"
+          >
+            Faça o login
+          </Link>
         </div>
-        <FormRegisterMessageError errors={errors} />
+        <FormRegisterMessageError errors={errors} errorAccess={errorAccess} />
       </Form>
     </div>
   );
@@ -138,9 +177,11 @@ const FormFieldPassword = ({
 
 type FormRegisterMessageErrorProps = {
   errors?: FieldErrors<Form>;
+  errorAccess?: { message: string };
 } & ComponentProps<"div">;
 const FormRegisterMessageError = ({
   errors,
+  errorAccess,
   ...props
 }: FormRegisterMessageErrorProps) => {
   const errorsArray = [
@@ -149,6 +190,7 @@ const FormRegisterMessageError = ({
     errors?.password?.message,
     errors?.email?.message,
     errors?.name?.message,
+    errorAccess?.message,
   ];
   return <FormMessageError errors={errorsArray} {...props} />;
 };
