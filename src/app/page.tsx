18a867872c127bbab4fa6dba4d75a3ff203a5965
Marcelo Oliveira <marcelomatheusbr@gmail.com>
2025-03-23 19:13:53 -0300

    feat: enhance home page with conditional rendering and logout functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
 "use client";
-import { useAuth } from "./context/Auth-Context";
+import { Button } from "@/components/ui/Button";
+import { useAuth } from "./_context/Auth-Context";
 
 export default function Home() {
-  const contextAuth = useAuth();
+  const auth = useAuth();
+  const logout = () => {
+    auth?.dispatch({ type: "LOGOUT" });
+  };
   return (
-    <div className="bg-dark  w-[600px]">
-      <h1 className="font-bold text-3xl flex font-heading">Home</h1>
-      <button onClick={() => contextAuth?.dispatch({ type: "LOGOUT" })}>
-        Logout
-      </button>
-    </div>
+    <>
+      { auth?.token ?
+        <div className="bg-dark  w-[600px]">
+          <h1 className="font-bold text-3xl flex font-heading">Home</h1>
+          <Button type="button" onClick={() => logout()}>
+            Logout
+          </Button>
+        </div>
+        :
+        <div>Direcionando para página de login</div>
+      }
+    </>
   );
 }
