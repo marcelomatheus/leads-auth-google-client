ceded7b8ebf87d2687e2b3f352675ffa7e0bbb34
Marcelo Oliveira <marcelomatheusbr@gmail.com>
2025-03-30 22:47:22 -0300

    feat: add Lottie animation for loading state on home page and improve layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/Button";
 import { useAuth } from "./_context/Auth-Context";
-
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 export default function Home() {
   const auth = useAuth();
   const logout = () => {
@@ -9,16 +9,26 @@ export default function Home() {
   };
   return (
     <>
-      { auth?.token ?
-        <div className="bg-dark  w-[600px]">
+      {auth?.token ? (
+        <div className="bg-dark w-full h-screen flex flex-col items-center justify-center">
+          <div className="w-[600px] h-[400px] bg-white p-5 rounded-lg flex flex-col items-center justify-center">
           <h1 className="font-bold text-3xl flex font-heading">Home</h1>
-          <Button type="button" onClick={() => logout()}>
+          <Button type="button" className="text-black" onClick={() => logout()}>
             Logout
           </Button>
+          </div>
+          
         </div>
-        :
-        <div>Direcionando para página de login</div>
-      }
+      ) : (
+        <div className="flex flex-col items-center justify-center h-screen w-full">
+          <DotLottieReact
+            src="https://lottie.host/8ba0afa3-daea-4c28-bd39-8dbce4de9c5a/pz9zvfY8s0.lottie"
+            loop
+            autoplay
+            className="w-[500px]"
+          />
+        </div>
+      )}
     </>
   );
 }
